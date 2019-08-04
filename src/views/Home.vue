5d8061158cb9e484a9e3237d823b10dfdf1d6ef3
1046633575 <1046633575@qq.com>
2019-08-04 11:33:19 +0800

    轮播图

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,53 @@
 <template>
   <div class="home">
-    首页
+    <swiper :options="swiperOption" ref="mySwiper" @someSwiperEvent="callback">
+      <!-- slides -->
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/0357494396aaf611469a652edd548274.jpeg" alt="">
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/0357494396aaf611469a652edd548274.jpeg" alt="">
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/0357494396aaf611469a652edd548274.jpeg" alt="">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination pagination-home text-right px-3 pb-1"  slot="pagination"></div>
+    
+  </swiper>
   </div>
 </template>
 
 <script>
-
+  export default {
+    data() {
+      return {
+        swiperOption: {
+          loop: true,
+          autoplay: {
+            delay: 1500
+          },
+          pagination: {
+            el: '.pagination-home',
+            
+          }
+        }
+      }
+    }
+  }
 </script>
+
+<style lang="scss">
+@import '../assets/scss/_variables.scss';
+
+  .pagination-home {
+    .swiper-pagination-bullet { 
+      border-radius: 0.1538rem;
+      background-color: map-get($colors, 'white');
+      opacity: 1;
+      &.swiper-pagination-bullet-active {
+        background-color: map-get($colors, 'info');
+      }
+    }
+  }
+</style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -28,10 +28,10 @@
 </template>
 <script>
 export default {
-    
+
 }
 </script>
 <style>
 
 </style>
-
+    
