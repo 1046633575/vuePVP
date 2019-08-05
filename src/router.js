b1fc598452ec07fac5c1f88d1fab80eb4c09c729
1046633575 <1046633575@qq.com>
2019-08-05 22:36:09 +0800

    英雄详情头部

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Main from './views/Main.vue'
 import Article from './views/Article.vue'
+import Hero from './views/Hero.vue'
 
 Vue.use(Router)
 
@@ -17,12 +18,8 @@ export default new Router({
       ]
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/hero',
+      component: Hero
     }
   ]
 })
---
+++ src/views/Hero.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="container" v-if="model">
+        <div class="topBar bg-dark-2 py-2 px-3 d-flex ai-center">
+            <img src="../assets/logo.png" height="30">
+            <div class="px-3">
+                <div class="text-white">王者荣耀</div>
+            </div>
+            <div class="ml-1 text-white flex-1">攻略站</div>
+            <router-link tag="div" to="/" class="text-white">更多英雄 &gt;</router-link>
+        </div>
+        <div class="top" :style="{'background-image': `url(${model.banner})`}">
+            <div class="info pb-3 text-white px-3 d-flex flex-column jc-end">
+                <div class="fs-sm">{{model.name2}}</div>
+                <div class="fs-xl py-1" style="font-weight: 700">{{model.name}}</div>
+                <div class="fs-sm py-1">{{model.type}}</div>
+                <div class="d-flex">
+                    <span class="pr-2">难度</span>
+                    <span class="circle px-1 bg-primary">{{model.property.difficulty}}</span>
+                    <span class="px-2">技能</span>
+                    <span class="circle px-1 bg-info">{{model.property.skill}}</span>
+                    <span class="px-2">攻击</span>
+                    <span class="circle px-1 bg-pink">{{model.property.attack}}</span>
+                    <span class="px-2">生存</span>
+                    <span class="circle px-1 bg-grey">{{model.property.defense}}</span>
+                    <span class="flex-1"></span>
+                    <router-link to="/" tag="div" class="text-grey">皮肤: {{model.num}} &gt;</router-link>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            model: null
+        }
+    },
+    methods: {
+        //获取英雄详情数据
+        getHero(){
+            this.$http.get('./data/hero.json').then(res => {
+                this.model = res.data;
+            })
+        }
+    },
+    created(){
+        this.getHero();
+    }
+}
+</script>
+
+<style lang="scss">
+.container{
+    .top{
+        height: 50vw;
+        background: #fff no-repeat top center;
+        background-size: auto 100%;
+        .info{
+            height: 100%;
+            background: linear-gradient(to top, rgba(0,0,0,1), rgba(0,0,0,.1));
+            .circle{
+                border-radius: 50%;
+            }
+        }
+    }
+}
+</style>
+
+
